5d0dd676b2ea8eca946309746388fd10b2bda78a
Hirvio <oscar.rehnstrom@hotmail.com>
2014-04-09 20:29:18 +0200

    asd

---
+++ main/java/arcanecraft/blocks/ArcanecraftBlocks.java
@@ -0,0 +1,13 @@
+package arcanecraft.blocks;
+
+import arcanecraft.main.Arcanecraft;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+
+/**
+ * Created by Hirvio on 2014-04-04.
+ */
+public class ArcanecraftBlocks {
+
+    public static final Block arcaniumScreen =  GameRegistry.findBlock(Arcanecraft.MODID, "arcaniumScreen");
+}
